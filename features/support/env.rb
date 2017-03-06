8d526010cdae364e10e535f43c7ca41ebffd4090
Gabriel Alvarado <jgalvaradoaguilar@gmail.com>
2017-03-06 13:37:33 +0100

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,9 +2,8 @@ require 'capybara/cucumber'
 require 'selenium-webdriver'
 require 'page-object'
 require 'page-object/page_factory'
-require 'rspec/retry'
 
-$TIMEOUT = 120
+$TIMEOUT = 15
 PageObject.default_page_wait = $TIMEOUT
 PageObject.default_element_wait = $TIMEOUT
 
@@ -15,18 +14,7 @@ Before do
   @browser = Selenium::WebDriver.for :firefox
   @browser.manage.timeouts.implicit_wait = $TIMEOUT
   @browser.manage.timeouts.page_load = 120
-  @browser.manage.window.maximize
-  
-  # Retry to open URL if it fails
-  RSpec.configure do |config|
-    # show retry status in spec process
-    config.verbose_retry = true
-    # Try twice (retry once)
-    config.default_retry_count = 2
-    # Only retry when Selenium raises Net::ReadTimeout
-    config.exceptions_to_retry = [Net::ReadTimeout]
-  end
-  
+  @browser.manage.window.maximize  
 end
 
 After do |scenario|
